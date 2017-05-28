79541237a393c4eaeb7abd87a8986071325f63b4
ea_pyl <gromkaktus@gmail.com>
2017-05-28 18:50:42 +0200

    Added GCD

---
+++ gcd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace gcd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GCD(24, 24));
+        }
+
+        static int GCD(int a, int b)
+        {
+            Debug.Assert(a > b, $"{nameof(a)} should be bigger than {nameof(b)}");
+            int remainder = 0;
+            while (b != 0)
+            {
+                remainder = a % b;
+                a = b;
+                b = remainder;
+            }
+            return a;
+        }
+    }
+}
