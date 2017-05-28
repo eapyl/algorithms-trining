1dd76ee17b42c99b0f2f15de57fd1bc50231d34d
ea_pyl <gromkaktus@gmail.com>
2017-05-28 19:16:22 +0200

    Added RaiseToPower

---
+++ raise-to-power/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace raise_to_power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RaiseToPower(7, 8));
+        }
+
+        static double RaiseToPower(double number, int power)
+        {
+            var bitMaskForPower = new BitArray(new int[] { power });
+            var previousNumberInPower2 = number;
+            var currentPowerOf2 = 1;
+            var result = 1.0;
+            for (var i = 0; i < bitMaskForPower.Length; i++)
+            {
+                if (bitMaskForPower[i])
+                {
+                    result *= previousNumberInPower2;
+                }
+                previousNumberInPower2 = previousNumberInPower2 * previousNumberInPower2;
+                if (currentPowerOf2 + 1 > power)
+                    break;
+            }
+            return result;
+        }
+    }
+}
