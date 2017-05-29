a269cbb865873655825106e18fa79b0909116f1c
ea_pyl <gromkaktus@gmail.com>
2017-05-29 18:19:36 +0200

    numeric integration  using trapeze

---
+++ numerical-integration-trapeze/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace numerical_integration_trapeze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IntegrationByTrapeze(x => x * x, 0, 10, 5));
+        }
+
+        static double IntegrationByTrapeze(Func<double, double> func, double xmin, double xmax, int number_intervals)
+        {
+            Debug.Assert(xmin < xmax);
+            var widthOfTrapeze = (xmax - xmin) / number_intervals;
+            var result = 0.0;
+            var x = xmin;
+            for (var i =0; i < number_intervals; i++)
+            {
+                result += widthOfTrapeze * (func(x) + func(x+widthOfTrapeze))/2;
+                x += widthOfTrapeze;
+            }
+            return result;
+        }
+    }
+}
