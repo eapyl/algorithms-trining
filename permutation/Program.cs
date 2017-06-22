f578b47cde375e6a81826909357a8c56814f81e9
ea_pyl <gromkaktus@gmail.com>
2017-06-22 19:31:58 +0200

    Added permutation using recursion

---
+++ permutation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace permutation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i =0;
+            foreach (var per in PermutateString("abcd"))
+                Console.WriteLine((i++) + per);
+
+        }
+
+        static IEnumerable<string> PermutateString(string s)
+        {
+            //Console.WriteLine(s);
+            if (s.Length == 1) yield return s;
+            if (s.Length > 1)
+            {
+                var ch = s[0].ToString();
+                foreach (var permutation in PermutateString(s.Substring(1)))
+                {
+                    for (var i = 0; i < permutation.Length+1; i++)
+                        yield return permutation.Insert(i, ch);
+                }
+            }
+        }
+    }
+}
---
+++ queue-insert-sort/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace queue_insert_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var q = new Queue<int>(new List<int> { 3, 5, 2, 1, 4 });
+            var sorted = QueueInsertSort(q);
+            while (sorted.Count > 0)
+                Console.WriteLine(sorted.Dequeue());
+        }
+
+        static Queue<int> QueueInsertSort(Queue<int> queue)
+        {
+            for (var i = 0; i < queue.Count; i++)
+            {
+                var temp = new Queue<int>(queue.Count);
+                for (var j = 0; j < i+1; j++)
+                    temp.Enqueue(queue.Dequeue());
+                for (var j = 0; j < i; j++)
+                    temp.Enqueue(temp.Dequeue());
+                while (queue.Count > 0)
+                {
+                    var item = queue.Dequeue();
+                    while (temp.Count > 0 && temp.Peek() < item)
+                    {
+                        queue.Enqueue(temp.Dequeue());
+                    }
+                    temp.Enqueue(item);
+                }
+                queue = temp;
+            }
+            return queue;
+        }
+    }
+}
